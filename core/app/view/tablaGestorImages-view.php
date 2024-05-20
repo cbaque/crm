84acb74d02d52aaeb2cd86209c8c21dc87e8a6f9
cbaque <claudio.sys16@gmail.com>
2024-05-20 16:48:02 -0500

    refactorizando codigo

--- core/app/view/tablaGestorImages-view.php
+++ core/app/view/tablaGestorImages-view.php
@@ -5,82 +5,11 @@
 				<h4 class="title">Gestor de Documentos</h4>
 			</div>
 			<div class="card-content table-responsive">
-				<!-- <a href="./index.php?view=newticket" class="btn btn-info">Nueva Categoria</a> -->
 				<?php $archivo = ArchivoData::getAll(); ?>
-
-				<!-- Modal -->
-				<!-- <span class="btn btn-info" id="btnarch">
-					Crear Archivo
-				</span>
-
-				<span class="btn btn-info" id="btndoc">
-					Agregar Documento
-				</span> -->
 
 				<a class="btn btn-default" href="?view=tablaGestor" >
 					Regresar	
 				</a>
-
-
-				<!-- Modal -->
-				<div class="modal fade" id="modalAgregarDocumento" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-					<div class="modal-dialog" role="document">
-						<div class="modal-content">
-							<div class="modal-header">
-								<h5 class="modal-title" id="myModalLabel">Subir documento</h5>
-							</div>
-							<div class="modal-body">
-								<form id="frmDocumentos" enctype="multipart/form-data">
-									<?php $archivo2 = ArchivoData::getAll(); ?>
-									<label>Archivo</label>
-									<select name="category_id" class="form-control">
-										<option value="">SELECCIONE</option>
-										<?php foreach ($archivo2 as $p) : ?>
-											<option value="<?php echo $p->id; ?>" <?php if (isset($_GET["category_id"]) && $_GET["category_id"] == $p->id) {
-																						echo "selected";
-																					} ?>><?php echo utf8_encode($p->nombre); ?></option>
-										<?php endforeach; ?>
-									</select>
-									<div class="modal-body">
-										<input type="text" name="namepac" required class="form-control" id="namepac" placeholder="Nombre">
-									</div>
-									<div class="modal-body">
-										<input type="text" name="cedulapac" required class="form-control" id="cedulapac" placeholder="Cedula">
-									</div>
-
-									<label>Selecciona Documento</label>
-									<input type="file" name="archivos[]" id="archivos" class="form-control" multiple="multiple">
-								</form>
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-								<button type="button" class="btn btn-primary" id="btnGuardarDocumentos">Guardar</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!--fin modal-->
-
-				<!-- Modal -->
-				<div class="modal fade" id="modalAgregarArchivos" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-					<div class="modal-dialog" role="document">
-						<div class="modal-content">
-							<div class="modal-header">
-								<h5 class="modal-title" id="myModalLabel">Crear Archivo</h5>
-							</div>
-							<div class="modal-body">
-								<form id="frmArchivos" enctype="multipart/form-data">
-									<input type="text" name="name" required class="form-control" id="name" placeholder="Nombre">
-								</form>
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-								<button type="button" class="btn btn-primary" id="btnGuardarArchivos">Guardar</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!--fin modal-->
 
 				<!-- Modal -->
 				<div class="modal fade" id="visualizarArchivo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -97,56 +26,8 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div>		
 
-				<div class="modal fade" id="viewArchivos" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-					<div class="modal-dialog modal-lg" role="document">
-						<div class="modal-content">
-							<div class="modal-header">
-								<h5 class="modal-title" id="exampleModalLabel">Listado de Im&aacute;genes</h5>
-							</div>
-							<div class="modal-body">
-								<div id="agrupacionArchivos"></div>
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-							</div>
-						</div>
-					</div>
-				</div>				
-
-				<br><br>
-				<!-- <form class="form-horizontal" role="form">
-					<input type="hidden" name="view" value="documentos">
-
-					<div class="form-group">
-						<div class="col-lg-2">
-							<div class="input-group">
-								<span class="input-group-addon"><i class="fa fa-search"></i></span>
-								<input type="text" name="q" value="<?php if (isset($_GET["q"]) && $_GET["q"] != "") {
-																		echo $_GET["q"];
-																	} ?>" class="form-control" placeholder="Nombre">
-							</div>
-						</div>
-						<div class="col-lg-2">
-							<div class="input-group">
-								<span class="input-group-addon"><i class="fa fa-th-list"></i></span>
-								<select name="category_id" class="form-control">
-									<option value="">ARCHIVO</option>
-									<?php foreach ($archivo as $p) : ?>
-										<option value="<?php echo $p->id; ?>" <?php if (isset($_GET["category_id"]) && $_GET["category_id"] == $p->id) {
-																					echo "selected";
-																				} ?>><?php echo utf8_encode($p->nombre); ?></option>
-									<?php endforeach; ?>
-								</select>
-							</div>
-						</div>
-
-						<div class="col-lg-2">
-							<button class="btn btn-primary btn-block">Buscar</button>
-						</div>
-					</div>
-				</form> -->
 				<?php
 				$data = array();
 				$data = DocumentoData::getByArchivoId($_GET["id"]);
